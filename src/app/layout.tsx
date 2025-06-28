e36d6cf5d74be6c855e55e0e3c36d7e8734e5103
Jaenudin Firdaus <jaenudinfirdaus2004@gmail.com>
2025-06-28 09:18:46 +0700

    feat: integrate sonner for notifications and refactor notes fetching in Card component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
-
+import { Toaster } from "sonner";
 const roboto = Roboto({
   subsets: ["latin"],
 });
@@ -18,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${roboto.className} antialiased`}>{children}</body>
+      <body className={`${roboto.className} antialiased`}>
+        {children}
+        <Toaster position="top-center" />
+      </body>
     </html>
   );
 }
--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,15 +1,52 @@
-import Note from "@/types/Note";
-import dateFormat from "@/utils/dateFormat";
+"use client";
+
+import React, { useEffect, useState } from "react";
+import Link from "next/link";
+import { toast } from "sonner";
 import { SquarePen, Trash } from "lucide-react";
 
-export default async function Card({}) {
-  const res = await fetch("http://localhost:3000/api/notes");
-  const notes = await res.json();
+import Note from "@/types/Note";
+import { URL } from "@/utils/config";
+import dateFormat from "@/utils/dateFormat";
+
+export default function Card({}) {
+  const [notes, SetNotes] = useState([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    const getNote = async () => {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${URL}/notes`, { cache: "force-cache" });
+        const data = await res.json();
+        SetNotes(data);
+      } catch (error) {
+        toast.error("Unable to load notes. Please try again.");
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    getNote();
+  }, []);
+
+  if (isLoading)
+    return (
+      <h1 className="text-3xl text-center mt-40">Fetching all notes...</h1>
+    );
+
+  const handleClick = (e: React.FormEvent) => {
+    e.preventDefault();
+  };
 
   return (
     <>
       {notes?.map((note: Note) => (
-        <div key={note.id} className="outline p-4 space-y-0.5 relative">
+        <Link
+          href={`/notes/${note.id}`}
+          key={note.id}
+          className="outline p-4 space-y-0.5 relative"
+        >
           <time className="text-xs text-gray-600 block">
             {dateFormat(note.createdAt)}
           </time>
@@ -21,12 +58,15 @@ export default async function Card({}) {
               {note.content}
             </p>
           </div>
-          <div className="flex items-center absolute right-3 bottom-1 space-x-1">
+          <div
+            className="flex items-center absolute right-3 bottom-1 space-x-1"
+            onClick={handleClick}
+          >
             <Trash className="size-5 cursor-pointer hover:text-red-500 duration-200 hover:-translate-y-0.5 hover:scale-110" />
             <span className="text-2xl text-gray-400 select-none">|</span>
             <SquarePen className="size-5 cursor-pointer hover:text-green-500 duration-200 hover:-translate-y-0.5 hover:scale-110" />
           </div>
-        </div>
+        </Link>
       ))}
     </>
   );
